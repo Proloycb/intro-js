65627681f58d666335e3fc7fea80ca2cc35ea0dd
Proloycb <proloychacroborty@gmail.com>
2022-02-13 13:50:59 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,11 @@
+console.log(87);
+
+var price = 25;
+var age = 21;
+var temp = 20;
+
+var school ='TPGHS';
+var address ="Tarail";
+
+var dineer = false;
+var night = true;
---
+++ variable-name.js
@@ -0,0 +1,11 @@
+var a = 5;
+var b = 7;
+var total = a+b;
+var minas = a-b;
+var multi = a*b;
+var div = a/b;
+
+console.log(total);
+console.log(minas);
+console.log(multi);
+console.log(div);
